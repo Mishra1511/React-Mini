43c01ebf4b053e418cfc5b3211373851f5e55e1f
Mishra1511 <shruti.mish1511@gmail.com>
2024-01-12 14:52:47 +0530

    new commit

---
+++ tester/src/App.js
@@ -0,0 +1,12 @@
+import {Button} from 'evergreen-ui';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React from app.js</h1>
+      <Button >Click Me!!</Button>
+    </div>
+  );
+}
+
+export default App;
