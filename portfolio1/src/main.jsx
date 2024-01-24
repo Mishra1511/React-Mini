c2154cfbe29dcdec107beaaa77159ee5192e84cf
Mishra1511 <shruti.mish1511@gmail.com>
2024-01-25 02:09:36 +0530

    Skills page added

--- portfolio1/src/main.jsx
+++ portfolio1/src/main.jsx
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Home from './component/Home.jsx'
+import MySkills from './component/MySkills.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NavBar />
     <Home />
+    <MySkills/>
   </React.StrictMode>,
 )
